6c512f071c3b6f2efb30af88200c3616e9253221
Tongshak <sabotongshak225@gmail.com>
2025-08-12 13:23:19 +0000

    refresher

---
+++ e2_variables.py
@@ -0,0 +1,122 @@
+ # 1
+s1 = "PythonProgramming"
+print(s1[0:6])
+print(s1[6:])
+print(s1[3:10])
+print(s1[:1])
+print(s1[10:17])
+
+# 2
+s2 = "DataScience"
+print(s2[:4])
+print(s2[4:])
+print(s2[1:7])
+print(s2[2:5])
+print(s2[5:])
+
+# 3
+s3 = "MachineLearning"
+print(s3[:7])
+print(s3[7:])
+print(s3[1:8])
+print(s3[3:9])
+print(s3[5:])
+
+# 4 (with space)
+s4 = "Artificial Intelligence"
+print(s4[:10])
+print(s4[10:])
+print(s4[1:11])
+print(s4[4:])
+print(s4[5:15])
+
+# 5
+s5 = "Blockchain"
+print(s5[:5])
+print(s5[5:])
+print(s5[1:4])
+print(s5[2:])
+print(s5[3:9])
+
+# 6
+s6 = "Cryptocurrency"
+s6[:6]
+s6[6:]
+s6[1:5]
+s6[2:8]
+s6[4:]
+
+# 7 (with space)
+s7 = "Programming Language"
+s7[:11]
+s7[11:]
+s7[1:8]
+s7[3:]
+s7[5:15]
+
+# 8
+s8 = "ComputerVision"
+s8[:8]
+s8[8:]
+s8[1:7]
+s8[3:]
+s8[4:12]
+
+# 9 (with space)
+s9 = "Deep Learning"
+s9[:4]
+s9[4:]
+s9[1:5]
+s9[2:]
+s9[3:10]
+
+# 10
+s10 = "NaturalLanguageProcessing"
+s10[:7]
+s10[7:]
+s10[1:9]
+s10[3:]
+s10[5:17]
+
+# 11
+s11 = "OpenAIChatbot"
+s11[:6]
+s11[6:]
+s11[1:4]
+s11[2:]
+s11[4:12]
+
+# 12
+s12 = "QuantumComputing"
+s12[:7]
+s12[7:]
+s12[1:9]
+s12[3:]
+s12[5:14]
+
+# 13
+s13 = "AugmentedReality"
+s13[:9]
+s13[9:]
+s13[1:8]
+s13[2:]
+s13[4:13]
+
+# 14 (with space)
+s14 = "Virtual Machine"
+s14[:7]
+s14[7:]
+s14[1:5]
+s14[2:]
+s14[4:12]
+
+# 15
+s15 = "CyberSecurity"
+s15[:5]
+s15[5:]
+s15[1:6]
+s15[2:]
+s15[4:11]
+
+
+
