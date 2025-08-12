d9124f4906fc452e2ca6ef5bc1a56ad7029a2deb
Tongshak <sabotongshak225@gmail.com>
2025-08-12 13:20:16 +0000

    refresher

---
+++ e1_variables.py
@@ -0,0 +1,41 @@
+"""
+num = 1
+print(num)
+print(id(num))
+
+first_name = "sabo"
+last_name = "tongshak"
+age = 28
+is_married = False
+height = 1.8
+print(first_name)
+print(last_name)
+print(age)
+print(is_married)
+print(height)
+
+num=1
+num -= 10
+num = num - 10
+
+num="1"
+num = num + str(2)
+print(num)
+print(type(num))
+
+d = 1/2
+e = -1//2
+print(d,type(d))
+print(e,type(e))
+
+name="Tanko"
+print(name[-3:-1])
+print(name[-1:-3])
+print(name[-3:-0])
+print(name[-3: ])
+"""
+a=True
+b=1.2
+c=a+b
+print(c)
+print(type(c))
